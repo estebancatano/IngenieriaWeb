559599fbf705c4d0cd4db5d5739273b70f7e4139
Esteban Cataño Escobar <esteban.catanoe@gmail.com>
2016-05-05 10:37:05 -0500

    DTO, archivos de mapeo y daos para las tablas Reserva y Prestamo

---
+++ LogicaNegocio/src/co/edu/udea/iw/dao/ReservaDAO.java
@@ -0,0 +1,59 @@
+package co.edu.udea.iw.dao;
+
+import java.util.List;
+
+import co.edu.udea.iw.dto.Reserva;
+import co.edu.udea.iw.exception.IWDaoException;
+
+/**
+ * Interface que define los metodos que va a proveer el dao de Reserva
+ * @author Esteban Cataño
+ * @author Vanesa Guzman
+ * @author Jeison Triana
+ * @version 1
+ */
+public interface ReservaDAO {
+	/**
+	 * Metodo para insertar una reserva en la base datos
+	 * @param reserva Objeto Reserva con la información de la reserva a insertar
+	 * @throws IWDaoException Manejador de excepciones personalizado para los daos
+	 */
+	public void insertar(Reserva reserva)throws IWDaoException;
+	
+	/**
+	 * Metodo para obtener todos las reservas en la base datos
+	 * @return Lista con todas las reservas almacenadas en la base de datos
+	 * @throws IWDaoException Manejador de excepciones personalizado para los daos
+	 */
+	public List<Reserva> obtener()throws IWDaoException;
+	
+	/**
+	 *  Metodo para modificar una reserva de la base de datos
+	 * @param reserva Objeto Reserva con la información de la reserva a modificar
+	 * @throws IWDaoException Manejador de excepciones personalizado para los daos
+	 */
+	public void modificar(Reserva reserva) throws IWDaoException;
+	
+	/**
+	 *  Metodo para eliminar una reserva de la base de datos
+	 * @param reserva Objeto Reserva con la información de la reserva a eliminar
+	 * @throws IWDaoException Manejador de excepciones personalizado para los daos
+	 */
+	public void eliminar(Reserva reserva) throws IWDaoException;
+	
+	/**
+	 *  Metodo para obtener una sola reserva de la base de datos
+	 * @param codigo Codigo de la reserva que se desea consultar
+	 * @return Objeto Reserva con la información de la reserva consultada
+	 * @throws IWDaoException Manejador de excepciones personalizado para los daos
+	 */
+	public Reserva obtener(String codigo) throws IWDaoException;
+	/**
+	 * Método para obtener las reservas por medio de una columna diferente a la clave primaria
+	 * @param nombreColumna Nombre de la columna por la cual se desea buscar
+	 * @param valorColumna Dato de la columna de las reservas que se desean obtener
+	 * @return Lista con las reservas que tiene el dato dado en la columna dada
+	 * @throws IWDaoException Manejador de excepciones personalizado para los daos
+	 */
+	public List<Reserva> obtener(String nombreColumna, String valorColumna) throws IWDaoException;
+}
---
+++ LogicaNegocio/src/co/edu/udea/iw/dto/Reserva.java
@@ -0,0 +1,159 @@
+package co.edu.udea.iw.dto;
+
+import java.util.Date;
+
+/**
+ * Clase DTO para los datos de la tabla Reserva
+ * @author Esteban Cataño
+ * @author Vanesa Guzman
+ * @author Jeison Triana
+ * @version 1
+ */
+public class Reserva {
+	/**
+	 * Codigo de la reserva
+	 * El id de la tabla, es auto_increment
+	 */
+	private Long codigo;
+	/**
+	 * Codigo del dispositivo que se quiere reservar
+	 */
+	private Long codigoEjemplarDispositivo;
+	/**
+	 * Consecutivo del ejemplar dentro de los existentes en el dispositivo
+	 */
+	private Integer consecutivoEjemplar;
+	/**
+	 * Nombre de usuario del investigador que hace la reserva
+	 */
+	private Usuario investigador;
+	/**
+	 * Fecha en la que el investigador realiza la reserva
+	 */
+	private Date fechaSolicitud;
+	/**
+	 * Fecha para la cual el investigador desea prestar el dispositivo
+	 */
+	private Date fechaPrestamo;
+	/**
+	 * Número de horas que desea prestar el dispositivo
+	 */
+	private Integer cantidadHoras;
+	/**
+	 * Estado de la reserva, si fue aprobada o no
+	 */
+	private String aprobado;
+	/**
+	 * Nombre de usuario del administrador que aprobó la reserva.
+	 * Si la reserva fue aprobada por el sistema, este campo llevaría null
+	 */
+	private Usuario administradorAprueba;
+	/**
+	 * @return the codigo
+	 */
+	public Long getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the codigoEjemplarDispositivo
+	 */
+	public Long getCodigoEjemplarDispositivo() {
+		return codigoEjemplarDispositivo;
+	}
+	/**
+	 * @param codigoEjemplarDispositivo the codigoEjemplarDispositivo to set
+	 */
+	public void setCodigoEjemplarDispositivo(Long codigoEjemplarDispositivo) {
+		this.codigoEjemplarDispositivo = codigoEjemplarDispositivo;
+	}
+	/**
+	 * @return the consecutivoEjemplar
+	 */
+	public Integer getConsecutivoEjemplar() {
+		return consecutivoEjemplar;
+	}
+	/**
+	 * @param consecutivoEjemplar the consecutivoEjemplar to set
+	 */
+	public void setConsecutivoEjemplar(Integer consecutivoEjemplar) {
+		this.consecutivoEjemplar = consecutivoEjemplar;
+	}
+	/**
+	 * @return the investigador
+	 */
+	public Usuario getInvestigador() {
+		return investigador;
+	}
+	/**
+	 * @param investigador the investigador to set
+	 */
+	public void setInvestigador(Usuario investigador) {
+		this.investigador = investigador;
+	}
+	/**
+	 * @return the fechaSolicitud
+	 */
+	public Date getFechaSolicitud() {
+		return fechaSolicitud;
+	}
+	/**
+	 * @param fechaSolicitud the fechaSolicitud to set
+	 */
+	public void setFechaSolicitud(Date fechaSolicitud) {
+		this.fechaSolicitud = fechaSolicitud;
+	}
+	/**
+	 * @return the fechaPrestamo
+	 */
+	public Date getFechaPrestamo() {
+		return fechaPrestamo;
+	}
+	/**
+	 * @param fechaPrestamo the fechaPrestamo to set
+	 */
+	public void setFechaPrestamo(Date fechaPrestamo) {
+		this.fechaPrestamo = fechaPrestamo;
+	}
+	/**
+	 * @return the cantidadHoras
+	 */
+	public Integer getCantidadHoras() {
+		return cantidadHoras;
+	}
+	/**
+	 * @param cantidadHoras the cantidadHoras to set
+	 */
+	public void setCantidadHoras(Integer cantidadHoras) {
+		this.cantidadHoras = cantidadHoras;
+	}
+	/**
+	 * @return the aprobado
+	 */
+	public String getAprobado() {
+		return aprobado;
+	}
+	/**
+	 * @param aprobado the aprobado to set
+	 */
+	public void setAprobado(String aprobado) {
+		this.aprobado = aprobado;
+	}
+	/**
+	 * @return the administradorAprueba
+	 */
+	public Usuario getAdministradorAprueba() {
+		return administradorAprueba;
+	}
+	/**
+	 * @param administradorAprueba the administradorAprueba to set
+	 */
+	public void setAdministradorAprueba(Usuario administradorAprueba) {
+		this.administradorAprueba = administradorAprueba;
+	}
+}
