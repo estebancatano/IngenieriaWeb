35e943bd7028b888461a5d738a034f9a8491128c
Esteban Cataño Escobar <esteban.catanoe@gmail.com>
2016-05-25 11:26:31 -0500

    Log4j en las excepciones

--- ServiciosWeb/src/co/edu/udea/iw/exception/IWDaoException.java
+++ ServiciosWeb/src/co/edu/udea/iw/exception/IWDaoException.java
@@ -1,24 +1,32 @@
 package co.edu.udea.iw.exception;
 
-public class IWDaoException extends Exception {
+import org.apache.log4j.Logger;
 
+
+public class IWDaoException extends Exception {
+	private Logger log = Logger.getLogger(IWDaoException.class);
+	
 	public IWDaoException() {
 		// TODO Auto-generated constructor stub
+		log.error("IWDaoException");
 	}
 
 	public IWDaoException(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
+		log.error(arg0);
 	}
 
 	public IWDaoException(Throwable arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
+		log.error(arg0);
 	}
 
 	public IWDaoException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
+		log.error(arg0,arg1);
 	}
 
 }
--- ServiciosWeb/src/co/edu/udea/iw/exception/IWServiceException.java
+++ ServiciosWeb/src/co/edu/udea/iw/exception/IWServiceException.java
@@ -1,26 +1,33 @@
 package co.edu.udea.iw.exception;
 
-public class IWServiceException extends Exception {
+import org.apache.log4j.Logger;
 
+public class IWServiceException extends Exception {
+	private Logger log = Logger.getLogger(IWServiceException.class);
+	
 	public IWServiceException() {
 		super();
 		// TODO Auto-generated constructor stub
+		log.error("IWServiceException");
 	}
 
 
 	public IWServiceException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
+		log.error(arg0,arg1);
 	}
 
 	public IWServiceException(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
+		log.error(arg0);
 	}
 
 	public IWServiceException(Throwable arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
+		log.error(arg0);
 	}
 
 }
