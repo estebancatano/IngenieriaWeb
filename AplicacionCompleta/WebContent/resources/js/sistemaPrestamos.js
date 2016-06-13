898817a9c263cb3dc4090ab87fe83be237d1ee83
VaneGuz <vaneguz27@gmail.com>
2016-06-13 12:38:39 -0500

    interfaz  de login listo

--- AplicacionCompleta/WebContent/resources/js/sistemaPrestamos.js
+++ AplicacionCompleta/WebContent/resources/js/sistemaPrestamos.js
@@ -1,3 +1,25 @@
 /**
  * 
  */
+
+var app = angular.module('prestamos', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl:'login.html',
+		controller: 'controllerLogin'
+	});
+	
+	
+}]);
+
+//Controlador de login.html
+app.controller('controllerLogin', function($scope){
+	
+	
+	
+});
+
+
+
+
