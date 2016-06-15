cbb65e5c731532f1d5941405a720e050f4af74fc
VaneGuz <vaneguz27@gmail.com>
2016-06-15 14:08:49 -0500

    Solicitud de reserva en proceso

--- AplicacionCompleta/WebContent/resources/js/controllers.js
+++ AplicacionCompleta/WebContent/resources/js/controllers.js
@@ -1,9 +1,9 @@
-
-var app = angular.module('prestamos.controller',[]);
+var app = angular.module('prestamos.controller', []);
 
 // Controlador de login.html
 // El servicio debe de estar en la misma página???????
-app.controller('controllerLogin', function($scope, Login, $location, $cookies,Factory) {
+app.controller('controllerLogin', function($scope, Login, $location, $cookies,
+		Factory) {
 
 	$scope.user = "";
 	$scope.password = "";
@@ -18,7 +18,6 @@ app.controller('controllerLogin', function($scope, Login, $location, $cookies,Fa
 				$scope.password = "";
 			} else {
 				Factory.login($scope.user, data.rol.codigo);
-				
 			}
 		});
 
@@ -26,31 +25,69 @@ app.controller('controllerLogin', function($scope, Login, $location, $cookies,Fa
 
 });
 
-//Controlador de indexInvestigador
+// Controlador de indexInvestigador
 app.controller('controllerInvestigador', function($scope, $location, $cookies) {
 	$scope.user = $cookies.nombreUsuario;
 	$scope.rol = 'Investigador';
-	$scope.mostrarDispositivos = function(){
+	$scope.mostrarDispositivos = function() {
 		$location.url('/listaDispositivos');
 	};
+
 });
-//Controlador de listarDispositivos
-app.controller('controllerListaDispositivos', function($scope, Dispositivo, $location, $cookies,Factory) {
-		$scope.user = $cookies.nombreUsuario;
-		$scope.rol = 'Investigador';
-		Dispositivo.obtenerDispositivos().success(function(data) {	
-			var lista = data.dispositivoDTOWS;
-			var l = lista.length;
-			console.log(lista);
-			console.log(l);
-			console.log(typeof(lista));
-			if(lista.length > 1){
-				$scope.dispositivos = lista;
-			}else{
-				var arr = [];
-				arr.push(lista);
-				console.log(arr);
-				$scope.dispositivos = arr;
-			}
+// Controlador de listarDispositivos
+app.controller('controllerListaDispositivos', function($scope, Dispositivo,
+		$location, $cookies, Factory) {
+	$scope.user = $cookies.nombreUsuario;
+	$scope.rol = 'Investigador';
+	Dispositivo.obtenerDispositivos().success(function(data) {
+		var lista = data.dispositivoDTOWS;
+		var l = lista.length;
+		console.log(lista);
+		console.log(l);
+		console.log(typeof (lista));
+		if (lista.length > 1) {
+			$scope.dispositivos = lista;
+		} else {
+			var arr = [];
+			arr.push(lista);
+			console.log(arr);
+			$scope.dispositivos = arr;
+		}
+	});
+	$scope.reservar = function() {
+		$location.url('/solicitudReserva');
+		console.log($scope.dispositivo);
+	}
+});
+
+app.controller('controllerSolicitud',
+		function($cookies, $location, $scope) {
+			$scope.user = $cookies.nombreUsuario;
+			$scope.rol = 'Investigador';
+			console.log($scope.dispositivo);
+
+			$scope.aceptar = function() {
+
+				var date;
+				date = new Date();
+				date = $scope.fechaPrestamo.getUTCFullYear()
+						+ '-'
+						+ ('00' + ($scope.fechaPrestamo.getUTCMonth() + 1))
+								.slice(-2)
+						+ '-'
+						+ ('00' + $scope.fechaPrestamo.getUTCDate()).slice(-2)
+						+ ' '
+						+ ('00' + $scope.fechaPrestamo.getUTCHours()).slice(-2)
+						+ ':'
+						+ ('00' + $scope.fechaPrestamo.getUTCMinutes())
+								.slice(-2)
+						+ ':'
+						+ ('00' + $scope.fechaPrestamo.getUTCSeconds())
+								.slice(-2);
+				var fecha = date.replace(' ', 'T');
+				console.log(fecha);
+				console.log($scope.numeroHoras);
+
+			};
+
 		});
-});
--- AplicacionCompleta/WebContent/resources/js/sistemaPrestamos.js
+++ AplicacionCompleta/WebContent/resources/js/sistemaPrestamos.js
@@ -1,4 +1,5 @@
-var app = angular.module('prestamos', [ 'ngRoute', 'ngCookies', 'prestamos.controller', 'prestamos.service' ]);
+var app = angular.module('prestamos', [ 'ngRoute', 'ngCookies',
+		'prestamos.controller', 'prestamos.service' ]);
 
 app.config([ '$routeProvider', function($routeProvider) {
 
@@ -24,7 +25,7 @@ app.config([ '$routeProvider', function($routeProvider) {
 	// Ruta para: Solicitar una reserva
 	$routeProvider.when('/solicitudReserva', {
 		templateUrl : 'investigador/solicitudReserva.html',
-		controller : 'controllerInvestigador'
+		controller : 'controllerSolicitud'
 	});
 
 	// Administrador
@@ -42,16 +43,12 @@ app.config([ '$routeProvider', function($routeProvider) {
 
 } ]);
 
-
-
-//se ejecuta cuando se inicia el modulo angular
-app.run(function($rootScope, Factory){
-  //Se ejecuta cada vez que cambia la ruta
-  $rootScope.$on('$routeChangeStart', function()
-  {
-      //llamamos a checkStatus, el cual lo hemos definido en la factoria auth
-      //la cu�l hemos inyectado en la acci�n run de la aplicaci�n
-      Factory.validarEstado();
-  });
+// se ejecuta cuando se inicia el modulo angular
+app.run(function($rootScope, Factory) {
+	// Se ejecuta cada vez que cambia la ruta
+	$rootScope.$on('$routeChangeStart', function() {
+		// llamamos a checkStatus, el cual lo hemos definido en la factoria auth
+		// la cu�l hemos inyectado en la acci�n run de la aplicaci�n
+		Factory.validarEstado();
+	});
 });
-
